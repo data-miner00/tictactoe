6210ecf66d6b81d0285234eca5b3a164212ad08e
Shaun Chong <pkay_@live.com>
2026-09-01 18:24:10 +0800

    refactor: Remove unused shaun.h

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "shaun.h"
 #include <stdbool.h>
 #include <string.h>
 #include "peripheral.h"
--- src/shaun.h
+++
@@ -1,6 +0,0 @@
-#ifndef SHAUN_H
-#define SHAUN_H
-
-int age = 28;
-
-#endif // SHAUN_H
